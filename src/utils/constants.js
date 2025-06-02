8bed09100e2783d6420735752a493e92b09190a3
RakshThimma <rakshithathimma@gmail.com>
2025-06-02 13:07:24 +0530

    http to https

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ const MY_API_KEY =process.env.REACT_APP_GOOGLE_YOUTUBE_API_KEY
 
 export const YOUTUBE_VIDEOS_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=9&key=" + MY_API_KEY
 
-export const YOUTUBE_SEARCH_URL ="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const YOUTUBE_SEARCH_URL ="https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
 export const YOUTUBE_COMMENTS_API = "https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId=pN6jk0uUrD8&key=" + MY_API_KEY
 
